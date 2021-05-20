255c9b710efffe1a422ac1d58f6614e2f2761ece
Virginijus03 <klimauskas@yahoo.com>
2021-05-20 21:15:01 +0300

    clock format

--- js/components/clock/Clock.js
+++ js/components/clock/Clock.js
@@ -1,40 +1,59 @@
 class Clock {
-    constructor(selector){
+    constructor(selector, targetDate) {
         this.selector = selector;
+        this.targetDate = targetDate;
+
         this.DOM = null;
 
         this.init();
     }
 
-    init(){
-        if(!this.isValidSelector()){
+    init() {
+        if (!this.isValidSelector()) {
             return false;
-
         }
-        this.render();
 
+        this.render();
     }
 
-    isValidSelector(){
-        if(typeof this.selector !== 'string'||
-            this.selector === ''){
-            console.error('ERROR');
+    isValidSelector() {
+        if (typeof this.selector !== 'string' ||
+            this.selector === '') {
+            console.error('ERROR: selector has to be non-empty string');
             return false;
         }
 
         this.DOM = document.querySelector(this.selector);
-        if(!this.DOM){
-            console.error('ERROR');
+        if (!this.DOM) {
+            console.error('ERROR: could not find an element by given selector');
             return false;
-
         }
+
         return true;
     }
-    render(){
-        const timeValues = [432, 9, 37, 39];
+
+    formatTime(timeValues) {
+        const updatedTime = [];
+
+        for (let i = 0; i < timeValues.length; i++) {
+            const time = timeValues[i];
+            if (i === 0 || time > 9) {
+                // kada pushinam originalu skaiciu
+                updatedTime.push(time);
+            } else {
+                // o kada pridedam nuli priekyje
+                updatedTime.push('0' + time);
+            }
+        }
+
+        return updatedTime;
+    }
+
+    render() {
+        const timeValues = this.formatTime([2, 9, 37, 39]);
         const labelValues = ['Days', 'Hours', 'Minutes', 'Seconds'];
         let HTML = '';
-    
+
         for (let i = 0; i < timeValues.length; i++) {
             HTML += `<div class="time">
                         <div class="value">${timeValues[i]}</div>
@@ -43,28 +62,23 @@ class Clock {
         }
 
         this.DOM.innerHTML = HTML;
-    
-
     }
-
 }
 
+// function Clock(selector) {
+//     const DOM = document.querySelector(selector);
+//     const timeValues = [432, 9, 37, 39];
+//     const labelValues = ['Days', 'Hours', 'Minutes', 'Seconds'];
+//     let HTML = '';
 
+//     for (let i = 0; i < timeValues.length; i++) {
+//         HTML += `<div class="time">
+//                     <div class="value">${timeValues[i]}</div>
+//                     <div class="label">${labelValues[i]}</div>
+//                 </div>`;
+//     }
 
-/*function Clock(selector) {
-    const DOM = document.querySelector(selector);
-    const timeValues = [432, 9, 37, 39];
-    const labelValues = ['Days', 'Hours', 'Minutes', 'Seconds'];
-    let HTML = '';
-
-    for (let i = 0; i < timeValues.length; i++) {
-        HTML += `<div class="time">
-                    <div class="value">${timeValues[i]}</div>
-                    <div class="label">${labelValues[i]}</div>
-                </div>`;
-    }
-
-    DOM.innerHTML = HTML;
-}*/
+//     DOM.innerHTML = HTML;
+// }
 
 export { Clock }
