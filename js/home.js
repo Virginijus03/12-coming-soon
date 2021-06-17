2593c035da9a53334d9f6e0fa0397d0f0d274121
Virginijus03 <klimauskas@yahoo.com>
2021-06-17 18:25:29 +0300

    viso projekto pakeitimas

--- js/home.js
+++ js/home.js
@@ -1,3 +1,20 @@
-//IMPORT
+// IMPORT
+import { Clock } from "./components/clock/Clock.js";
+import { socials } from "./components/socials/socials.js";
+import { clockData } from "./data/clockData.js";
+import { socialsData } from "./data/socialsData.js";
 
-//EXECUTION
+// EXECUTION
+new Clock('#clock_1', clockData);
+socials('footer .socials', socialsData);
+
+/*
+import { Clock } from "./components/clock/Clock.js";
+import { socials } from "./components/socials/socials.js";
+import { socialsData } from "./data/socialsData.js";
+// EXECUTION
+new Clock('#clock_1', clockData);
+socials('footer .socials', socialsData);
+/*new Clock('#clock_1');
+// new Clock(485545);
+socials('footer .socials', socialsData);*/
