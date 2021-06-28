850098cf86e7d46266cfcc7a480a9b7992d5b05a
Virginijus03 <77634732+Virginijus03@users.noreply.github.com>
2021-06-28 21:39:36 +0300

    Toast in action

---
+++ js/components/form/Form.js
@@ -0,0 +1,128 @@
+class Form {
+    constructor(selector) {
+        this.selector = selector;
+
+        this.formDOM = null;
+        this.allInputsDOM = [];
+        this.submitButtonDOM = null;
+
+
+        this.init();
+    }
+
+    init() {
+        if (!this.isValidSelector()) {
+            console.error('ERROR: nevalidus selektorius!');
+            return false;
+        }
+        this.formDOM = document.querySelector(this.selector);
+        if (!this.formDOM) {
+            console.error('ERROR:nerastas formos elementas!')
+            return false;
+        }
+        this.allInputsDOM = this.formDOM.querySelectorAll('input, textarea');
+        this.submitButtonDOM = this.formDOM.querySelector('button[type="submit"]');
+        //console.log(this.allInputsDOM);
+        this.addEvents();
+    }
+
+    isValidSelector() {
+        if (typeof this.selector !== 'string' ||
+            this.selector === '') {
+        }
+        return true;
+    }
+
+
+    isValidName(name) {
+        if (name === undefined ||
+            typeof name !== 'string' ||
+            name.length < 2 ||
+            !this.isUpperCase(name[0])) {
+            return false;
+        }
+        return true;
+    }
+
+    isValidLastName(lastName) {
+        if (lastName === undefined ||
+            typeof lastName !== 'string' ||
+            lastName.length < 2 ||
+            !this.isUpperCase(lastName[0])) {
+            return false;
+        }
+        return true;
+    }
+
+    isValidEmail(email) {
+        if (typeof email !== 'string' ||
+            email.length < 6 ||
+            email.indexOf('@') === -1 ||
+            email[0] === '@' ||
+            email.slice(-4).indexOf('@') > -1 ||
+            this.countSimbols(email, '@') > 1) {
+            return false;
+        }
+        return true;
+    }
+
+    isValidMessage(msg) {
+        if (typeof msg !== 'string' ||
+            msg === '') {
+            return false;
+        }
+        return true;
+    }
+    countSimbols(text, letter) {
+        let count = 0;
+
+        for (const t of text) {
+            if (t === letter) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+    isUpperCase(letter) {
+        return letter === letter.toUpperCase();
+    }
+
+    addEvents() {
+        this.submitButtonDOM.addEventListener('click', (e) => {
+            e.preventDefault();
+            let allGood = true;
+            for (let element of this.allInputsDOM) {
+                const validationRule = element.dataset.validation;
+
+                if (validationRule === 'email') {
+                    if (this.isValidEmail(element.value) === false) {
+                        allGood = false;
+                        console.error('ERROR: nera email adreso!');
+                        break;
+                    }
+                }
+                if (validationRule === 'name') {
+                    if (this.isValidName(element.value) === false) {
+                        allGood = false;
+                        console.error('ERROR: nera vardo!');
+                        break;
+                    }
+                }
+                if (validationRule === 'text') {
+                    if (this.isValidMessage(element.value) === false) {
+                        allGood = false;
+                        console.error('ERROR: nera zinutes!');
+                        break;
+                    }
+                }
+            }
+            //console.log('All Good?', allGood)
+
+
+        });
+
+    }
+}
+
+export { Form }
--- js/home.js
+++ js/home.js
@@ -1,18 +1,31 @@
 // IMPORT
 import { Clock } from "./components/clock/Clock.js";
 import { Progressbar } from "./components/progress-bar/ProgressBar.js";
-import { socials } from "./components/socials/socials.js";
+//import { socials } from "./components/socials/socials.js";
 import { clockData } from "./data/clockData.js";
 import { progressBarData } from "./data/progressBarData.js";
 import { socialsData } from "./data/socialsData.js";
 import { SocialsOOP } from "./components/socials/SocialsOOP.js";
-
+import { Form } from "./components/form/Form.js";
+import { Toast } from "./components/toast/Toast.js";
 
 // EXECUTION
+const toast = new Toast;
+// toast.success('Tau pavyko!');
+// toast.info('Pranesame apie si bei ta!');
+// toast.warning('Tu cia ziurek..!');
+
+// toast.error('Yra klaida!', 'Nauja antraste');
+toast.error('Yra klaida!');
+// toast.error();
+
 new Clock('#clock_1', clockData);
 //socials('footer .socials', socialsData);
 new Progressbar('.left-column', progressBarData);
 new SocialsOOP('.row-big.socials', socialsData);
+new Form('.hero form');
+new Form('main form');
+
 
 /*
 import { Clock } from "./components/clock/Clock.js";
